a6c405c7a3cdee9252eee1b2dcb1c71f2adabdb0
Prasanth Vinnakota <crazyprasanth16@gmail.com>
2019-05-17 14:35:59 +0530

    Write a generator function which returns the Fibonacci series.

---
+++ generator-fibonacci.py
@@ -0,0 +1,23 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+
+    for i in range(n):
+        yield a
+        c = a + b
+        a = b
+        b = c
+
+
+size = None
+try:
+    size = int(input("Enter size of fibonacci series: "))
+    if size == 0:
+        raise ValueError
+
+except ValueError:
+    print("input must be a number and greater than 0")
+    exit(0)
+
+for j in fibonacci(size):
+    print(j, end=" ")
